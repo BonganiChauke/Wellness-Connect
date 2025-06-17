479cdf44fab5be2f279e201590578f1f55d7ecdd
BonganiChauke <bonganichauke11@outlook.com>
2025-06-17 15:07:17 +0200

    Refactor form validation logic and enhance user feedback for input fields

--- javascript/script.js
+++ javascript/script.js
@@ -1,72 +1,97 @@
-var regex_Password = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,450}$/;
-var regex_Numbers = "[0-9]+";
-var regex_SpecialCharacter = /[-’/`~!#*$@_%+=.,^&(){}[\]|;:”<>?\\]/g;
-var regex_Email = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+// declaring variables for the form inputs fields
+const first_name = document.getElementById("first_name");
+const last_name = document.getElementById("last_name");
+const email = document.getElementById("email");
+const message = document.getElementById("message");
+const successAlert = document.getElementById('alert-success');
 
-var form = document.getElementById('form')
+function showError(input, message) {
+    const sibling = input.nextElementSibling; // This is the <label> element
+    const errorTxt = sibling ? sibling.nextElementSibling.querySelector('.error-msg') : null;
 
-form.addEventListener('submit', function (e) {
-
-    e.preventDefault()
-
-    var firstName = document.getElementById('firstName');
-    var lastName = document.getElementById('lastName');
-    var email = document.getElementById('reg_Email');
-    var password = document.getElementById('reg_Password');
-    var con_password = document.getElementById('confirm_Password');
-
-
-    if (firstName.value == "" && lastName.value == "" && email.value == "" && password.value == "" && con_password.value == "") {
-
-        alert("Please fill in the registration form")
-
-    } else if (firstName.value == "" || firstName.value.match(regex_Numbers) || firstName.value.length < 3 || firstName.value.match(regex_SpecialCharacter)) {
-
-        document.getElementById('firstName').focus()
-
-    } else if (lastName.value == "" || lastName.value.match(regex_Numbers) || lastName.value.length < 3 || lastName.value.match(regex_SpecialCharacter)) {
-
-        document.getElementById('lastName').focus()
-
-    } else if (email.value == "" || !email.value.match(regex_Email)) {
-
-        document.getElementById('reg_Email').focus()
-
-    } else if (password.value == "" || !password.value.match(regex_Password)) {
-
-        document.getElementById('reg_Password').focus()
+    if (!errorTxt) {
+        console.error('No element with the class ".error-msg" found for the input:', input);
+        return;
     }
-    else if (con_password.value == "") {
 
-        document.getElementById('confirm_Password').focus()
-
-    } else if (password.value != con_password.value) {
-
-        alert("Password does not match")
+    errorTxt.textContent = message;
+    errorTxt.parentElement.style.visibility = message ? 'visible' : 'hidden';
+}
 
 
+
+// input error 
+function inputError(input) {
+    input.style.border = '1px solid red';
+}
+
+//input success
+function inputSuccess(input) {
+    input.style.border = '1px solid green';
+}
+
+// show success alert
+function showSuccessAlert() {
+
+    if (successAlert) {
+        successAlert.style.display = 'block';
+        setTimeout(() => {
+            successAlert.style.display = 'none';
+        }, 5000);
+    }
+}
+
+// show error alert
+function showErrorAlert(message) {
+    const errorAlert = document.getElementById('alert-danger');
+    const errorMessageAlert = document.getElementById('error-message');
+    if (errorAlert) {
+
+        if (errorMessageAlert) {
+            errorMessageAlert.textContent = message;
+            errorAlert.style.display = 'block';
+            setTimeout(() => {
+                errorAlert.style.display = 'none';
+            }, 3000);
+        }
+    }
+}
+
+// function to validate user names field
+function validateNames(name, userName) {
+    if (name == '') return `${userName} can not be empty`
+    if (/[^A-Za-z]/.test(name)) return `${userName} must contain only letters`
+    if (name.length < 3) return `${userName} must be at least 3 characters long`
+    if (name.length > 50)
+        return `${userName} must can not exceed 50 characters long.`
+    return ''
+}
+
+// event on focus listener for user first name field
+first_name.addEventListener('focusout', function () {
+    const errorMessage = validateNames(first_name, 'First Name')
+    showError(first_name, errorMessage)
+    console.log("error... " + showError(first_name, errorMessage));
+
+    // to check the border if inputs are valid
+    if (errorMessage == '') {
+        inputSuccess(first_name);
+    } else {
+        inputError(first_name);
+    }
+});
+
+// validation function for the form inputs
+document.getElementById("contact-form").addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    //
+    if (validateNames(first_name.value)) {
+        alert("First Name is invalid. Please check your input.");
     } else {
 
-        form.reset();
-        alert("Successful Student Registration, Welcome to Wellness Connect ")
-
+        showSuccessAlert();
+        alert("Form submitted successfully!");
     }
-
-
-})
-
-
-// You can also pass an optional settings object
-// below listed default settings
-AOS.init({  
-
-  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  offset: 120, // offset (in px) from the original trigger point
-  delay: 0, // values from 0 to 3000, with step 50ms
-  duration: 900, // values from 0 to 3000, with step 50ms
-  easing: 'ease', // default easing for AOS animations
-  once: false, // whether animation should happen only once - while scrolling down
-  mirror: false, // whether elements should animate out while scrolling past them
-  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-
 });
+

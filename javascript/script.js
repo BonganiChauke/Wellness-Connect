4b95b45d46dc570e979b51d44806f44f5593fa42
BonganiChauke <bonganichauke11@outlook.com>
2025-06-27 12:56:45 +0200

    Add password toggle functionality to the form

--- javascript/script.js
+++ javascript/script.js
@@ -165,6 +165,18 @@ message.addEventListener('focusout', function () {
     }
 });
 
+// *********************************************************
+// password toggle function
+const togglePassword = document.getElementById('togglePassword');
+const passwordInput = document.getElementById('password');
+
+togglePassword.addEventListener('click', function () {
+    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordInput.setAttribute('type', type);
+    this.classList.toggle('fa-eye');
+    this.classList.toggle('fa-eye-slash');
+});
+
 // validation function for the form inputs
 document.getElementById("contact-form").addEventListener("submit", function (event) {
     event.preventDefault(); // Prevent the default form submission
